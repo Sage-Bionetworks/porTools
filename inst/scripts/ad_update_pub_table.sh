382bd55629a4627b3f81d90915e0a9d67725311b
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2022-01-06 13:23:44 -0800

    convenience scripts for testing

---
+++ inst/scripts/ad_update_pub_table.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+Rscript update-publications-ad.R --grant_table syn17024229 --parent syn20463015 --pub_table syn20448807
---
+++ inst/scripts/test_update_pub_table.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+Rscript update-publications-ad.R --grant_table syn17024229 --parent syn26339941 --pub_table syn26339943
